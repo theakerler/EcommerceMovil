e27764fd8451bfe095cfa928453014ead7d70922
Alexander EP <palex2143@gmail.com>
2025-06-15 22:57:39 -0500

    Add color definitions for UI theme in Color.kt and colors.xml

---
+++ app/src/main/java/com/proyecto/ecommercemovil/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.proyecto.ecommercemovil.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val Red50 = Color(0xFFFef2f2)
+val Red100 = Color(0xFFFee2e2)
+val Red200 = Color(0xFFFecaca)
+val Red300 = Color(0xFFfca5a5)
+val Red400 = Color(0xFFf87171)
+val Red500 = Color(0xFFef4444)
+val Red600 = Color(0xFFdc2626)
+val Red700 = Color(0xFFb91c1c)
+val Red800 = Color(0xFF991b1b)
+val Red900 = Color(0xFF7f1d1d)
+val Red950 = Color(0xFF450a0a)
