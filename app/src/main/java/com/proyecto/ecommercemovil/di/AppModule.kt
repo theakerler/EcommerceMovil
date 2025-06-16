c69cc2ed54572f48c46facebec709b38fed624a9
Alexander EP <palex2143@gmail.com>
2025-06-15 23:21:21 -0500

    Add AppModule and AppNavGraph for dependency injection and navigation setup

---
+++ app/src/main/java/com/proyecto/ecommercemovil/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.proyecto.ecommercemovil.di
+
+import android.app.Application
+import com.proyecto.ecommercemovil.data.network.AuthApiService
+import com.proyecto.ecommercemovil.data.network.CarritoApiService
+import com.proyecto.ecommercemovil.data.network.PrendaApiService
+import com.proyecto.ecommercemovil.data.network.UsuarioApiService
+import com.proyecto.ecommercemovil.data.repository.AuthRepository
+import com.proyecto.ecommercemovil.data.repository.CarritoRepository
+import com.proyecto.ecommercemovil.ui.viewmodel.carrito.CarritoViewModelFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object AppModule {
+    private const val BASE_URL = "http://192.168.216.135:8080/"
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val authApiService: AuthApiService = retrofit.create(AuthApiService::class.java)
+    val usuarioApiService: UsuarioApiService = retrofit.create(UsuarioApiService::class.java)
+    val prendaApiService: PrendaApiService = retrofit.create(PrendaApiService::class.java)
+    val carritoApiService: CarritoApiService = retrofit.create(CarritoApiService::class.java)
+
+    lateinit var authRepository: AuthRepository
+    lateinit var carritoRepository: CarritoRepository
+
+    // Agrega la factory del ViewModel de carrito
+    val carritoViewModelFactory: (Application) -> CarritoViewModelFactory = { application ->
+        CarritoViewModelFactory(application, carritoApiService, carritoRepository)
+    }
+    fun init(context: android.content.Context) {
+        authRepository = AuthRepository(authApiService, context)
+        carritoRepository = CarritoRepository(carritoApiService, context)
+    }
+}
---
+++ app/src/main/java/com/proyecto/ecommercemovil/ui/navigation/AppNavGraph.kt
@@ -0,0 +1,151 @@
+package com.proyecto.ecommercemovil.ui.navigation
+
+import LoginScreen
+import android.app.Application
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.proyecto.ecommercemovil.di.AppModule
+import com.proyecto.ecommercemovil.ui.screens.MainScreen
+import com.proyecto.ecommercemovil.ui.screens.botomNavScreen.HomeScreen
+import com.proyecto.ecommercemovil.ui.screens.botomNavScreen.TruckScreen
+import com.proyecto.ecommercemovil.ui.screens.prenda.HombreScreen
+import com.proyecto.ecommercemovil.ui.screens.prenda.InfantilScreen
+import com.proyecto.ecommercemovil.ui.screens.prenda.MujerScreen
+import com.proyecto.ecommercemovil.ui.screens.prenda.PrendaDetalleScreen
+import com.proyecto.ecommercemovil.ui.screens.prenda.PrendasDescuentoScreen
+import com.proyecto.ecommercemovil.ui.screens.procesoCompra.CarritoScreen
+import com.proyecto.ecommercemovil.ui.screens.sesion.LoginViewModel
+import com.proyecto.ecommercemovil.ui.screens.sesion.RegisterScreen
+import com.proyecto.ecommercemovil.ui.viewmodel.LoginViewModelFactory
+import com.proyecto.ecommercemovil.ui.viewmodel.RegisterViewModel
+import com.proyecto.ecommercemovil.ui.viewmodel.RegisterViewModelFactory
+import com.proyecto.ecommercemovil.ui.viewmodel.carrito.CarritoViewModel
+import com.proyecto.ecommercemovil.ui.viewmodel.prenda.PrendaDescuentoViewModel
+import com.proyecto.ecommercemovil.ui.viewmodel.prenda.PrendaDescuentoViewModelFactory
+import com.proyecto.ecommercemovil.ui.viewmodel.prenda.PrendaDetalleViewModel
+import com.proyecto.ecommercemovil.ui.viewmodel.prenda.PrendaDetalleViewModelFactory
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController = rememberNavController(),
+    activity: ComponentActivity
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "main" // Cambia a "main" si el usuario ya está autenticado
+    ) {
+
+        composable("main") {
+            MainScreen(navController) {
+                Text("Contenido principal")
+            }
+        }
+
+        composable("login") {
+            val factory = LoginViewModelFactory(AppModule.authRepository)
+            val loginViewModel: LoginViewModel = viewModel(factory = factory)
+            LoginScreen(
+                loginViewModel = loginViewModel,
+                navController = navController,
+                activity = activity,
+                onLoginSuccess = {
+                    navController.navigate("main") {
+                        popUpTo("login") { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable("register") {
+            // Si necesitas un factory personalizado para RegisterViewModel:
+             val factory = RegisterViewModelFactory(AppModule.usuarioApiService)
+             val registerViewModel: RegisterViewModel = viewModel(factory = factory)
+             RegisterScreen(navController, registerViewModel)
+        }
+        composable("mujer") {
+            MainScreen(navController) {
+                MujerScreen(navController)
+            }
+        }
+        composable("hombre") {
+            MainScreen(navController) {
+                HombreScreen(navController)
+            }
+        }
+        composable("infantil") {
+            MainScreen(navController) {
+                InfantilScreen(navController)
+            }
+        }
+        composable("descuentos/{categoria}") { backStackEntry ->
+            val categoria = backStackEntry.arguments?.getString("categoria") ?: ""
+            val factory = PrendaDescuentoViewModelFactory(AppModule.prendaApiService)
+            val prendaDescuentoViewModel: PrendaDescuentoViewModel = viewModel(factory = factory)
+
+            MainScreen(navController) {
+                PrendasDescuentoScreen(
+                    navController = navController,
+                    viewModel = prendaDescuentoViewModel,
+                    categoria = categoria
+                )
+            }
+        }
+
+        composable("prenda_detalle/{id}/{descuento}") { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id")?.toIntOrNull() ?: return@composable
+            val descuento = backStackEntry.arguments?.getString("descuento")?.toFloatOrNull() ?: 0f
+            val factory = PrendaDetalleViewModelFactory(
+                AppModule.prendaApiService,
+                AppModule.authRepository,
+                AppModule.carritoRepository
+            )
+            val viewModel: PrendaDetalleViewModel = viewModel(factory = factory)
+            MainScreen(navController) {
+                PrendaDetalleScreen(
+                    navController = navController,
+                    viewModel = viewModel,
+                    prendaId = id,
+                    descuento = descuento
+                )
+            }
+        }
+
+        composable("carrito/{carritoId}") { backStackEntry ->
+            val carritoId = backStackEntry.arguments?.getString("carritoId")?.toIntOrNull() ?: return@composable
+            val context = LocalContext.current
+            val application = context.applicationContext as Application
+            val carritoViewModel: CarritoViewModel = viewModel(
+                factory = AppModule.carritoViewModelFactory(application)
+            )
+            MainScreen(navController) {
+                CarritoScreen(
+                    carritoId = carritoId,
+                    viewModel = carritoViewModel
+                )
+            }
+        }
+
+
+        composable("home") {
+            MainScreen(navController) {
+                HomeScreen()
+            }
+        }
+        composable("envios") {
+            MainScreen(navController) {
+                TruckScreen()
+            }
+        }
+
+
+
+        // Agrega más destinos aquí si los necesitas
+    }
+}
